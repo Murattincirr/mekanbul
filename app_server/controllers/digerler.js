268c160f730e9f16ce4866a01c65477575ba19ee
Murattincirr <92016710+Murattincirr@users.noreply.github.com>
2022-10-21 10:47:09 +0300

    Delete app_server directory

--- app_server/controllers/digerler.js
+++
@@ -1,7 +0,0 @@
-const hakkinda = function(req,res,next){
-    res.render("hakkinda",{title:"Hakkinda sayfasi"})
-};
-
-module.exports.hakkinda=function(req,res){
-    res.render('hakkinda',{'title':Hakkında})
-};
